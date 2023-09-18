ddf261e6e135b3975518226872687e02539f4aeb
Kiarash Rahmani <microkia1928@gmail.com>
2023-09-18 22:11:46 +0330

    Update Find the Runner-Up Score!.py

--- 09-Find the Runner-Up Score!/Find the Runner-Up Score!.py
+++ 09-Find the Runner-Up Score!/Find the Runner-Up Score!.py
@@ -1,12 +1,11 @@
-n = int(input())
-arr = list(map(int, input().split()))
+n = int(input())  # Input: Length of the array
+arr = list(map(int, input().split()))  # Input: Array elements
 
-arr.sort(reverse=True)
+arr.sort(reverse=True)  # Sort the array in descending order
 
-
-i = 0
-while arr[i] == arr[i+1]:
+i = 0  # Counter variable
+while arr[i] == arr[i+1]:  # Find the first unique element
     i += 1
 
-result = arr[i+1]
-print(result)
+result = arr[i+1]  # Next unique element after the repeated ones
+print(result)  # Output: Result
